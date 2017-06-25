903a99a91f92d2553529af4b23521e80ff8615ac
Andrew Moll <mollandrew913@gmail.com>
2017-06-25 17:10:31 +0800

    Removed the logging in the controller and created a migration for the users table

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -3,12 +3,6 @@ defmodule Discuss.AuthController do
   plug Ueberauth
 
   def callback(conn, params) do
-    IO.puts "+++++++"
-    IO.inspect(conn.assigns)
-    IO.puts "+++++++"
-    IO.inspect(params)
-    IO.puts "+++++++"
-
-    conn
+    
   end
 end
