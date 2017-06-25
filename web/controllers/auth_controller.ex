88d8d3ebb2a99a64e34cb39f3238c82aa65fe9c0
Andrew Moll <mollandrew913@gmail.com>
2017-06-25 18:13:20 +0800

    Created helper function to decide if user should be inserted or updated

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -9,5 +9,16 @@ defmodule Discuss.AuthController do
 		    email: auth.info.email,
 		    provider: provider}
     changeset = User.changeset(%User{}, user_params)
+
+    insert_or_update_user(changeset)
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil - >
+	Repo.insert(changeset)
+      user ->
+	{:ok, user}
+    end
   end
 end
