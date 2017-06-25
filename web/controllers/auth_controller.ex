fb73d8fc311010a0810c6985bff294d85fce6e88
Andrew Moll <mollandrew913@gmail.com>
2017-06-25 17:58:59 +0800

    Modified controller to extract the token, email, and provider

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -2,7 +2,12 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
 
-  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
-    
+  alias Discuss.User
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => provider} = params) do
+    user_params = %{token: auth.credentials.token,
+		    email: auth.info.email,
+		    provider: provider}
+    changeset = User.changeset(%User{}, user_params)
   end
 end
