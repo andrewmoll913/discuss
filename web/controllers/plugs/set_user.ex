06e646c1d15be64614b7c36155de26b3772f3397
Andrew Moll <mollandrew913@gmail.com>
2017-06-26 20:50:53 +0800

    Added a new plug for setting the user

---
+++ web/controllers/plugs/set_user.ex
@@ -0,0 +1,12 @@
+defmodule Discuss.Plugs.SetUser do
+  import Plug.Conn
+  import Phoenix.Controller
+
+  alias Discuss.Repo
+  alias Discuss.User
+  alias Discuss.Router.Helper
+
+  def init(_params) do
+    
+  end
+end
